7bd31f48b543bdf1d22bc43f2c4bac4e181b3e20
Judy L <25380136+jjliem@users.noreply.github.com>
2021-05-18 14:26:02 -0500

    header css in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,34 @@ import React from "react";
 export default function Header(props) {
   const { countCartItems } = props;
   return (
-    <header className="row block center">
+    <header className="wrap_header">
       <div>
-        <a href="#/">
+        <a href="#/" className="nav-link">
           <h1>Paws n Play</h1>
         </a>
       </div>
-      <div>
-        <a href="#/cart">
+      <div id="menu-outer">
+        <div class="table">
+          <ul id="horizontal-list">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            
+          </ul>
+        </div>
+      </div>
+
+      <div className="header-icons">
+      <a href="#/cart" className="nav-link">
           My Cart{" "}
           {countCartItems ? (
             <button className="badge">{countCartItems}</button>
           ) : (
             ""
           )}
-        </a>{" "}
-        <a href="#/signin">Sign In</a>
+        </a>
       </div>
+
     </header>
   );
 }
