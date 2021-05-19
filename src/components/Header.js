1433c478a3c1d93d41561376fceff2c4f04c6494
Judy L <25380136+jjliem@users.noreply.github.com>
2021-05-19 13:09:00 -0500

    add center navbar no flexbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,28 @@ import React from "react";
 export default function Header(props) {
   const { countCartItems } = props;
   return (
-    <header className="wrap_header">
-      <div>
-        <a href="#/" className="nav-link">
-          <h1>Paws n Play</h1>
-        </a>
-      </div>
-      <div id="menu-outer">
-        <div class="table">
-          <ul id="horizontal-list">
-            <li><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            
-          </ul>
+      <div className="header">
+        <a href="#default" className="logo">Paws N Play</a>
+        <div className="header-center">
+          <a href="#home">Home</a>
+          <a href="#shop">Shop</a>
+          <a href="#sale">Sale</a>
+          <a href="#features">Features</a>
+          <a href="#blog">Blog</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+        </div>
+        <div className="header-right">
+        <a href="#/cart" className="nav-link">
+            My Cart{" "}
+            {countCartItems ? (
+              <button className="badge">{countCartItems}</button>
+            ) : (
+              ""
+            )}
+          </a>
         </div>
       </div>
 
-      <div className="header-icons">
-      <a href="#/cart" className="nav-link">
-          My Cart{" "}
-          {countCartItems ? (
-            <button className="badge">{countCartItems}</button>
-          ) : (
-            ""
-          )}
-        </a>
-      </div>
-
-    </header>
   );
 }
