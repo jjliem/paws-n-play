757e92a27852fd393a404414d7803d6e91057c96
Judy L <25380136+jjliem@users.noreply.github.com>
2021-05-20 22:00:49 -0500

    add github pages script, change shippingPrice

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,7 @@ export default function Cart(props) {
   const { cartItems, onAdd, onRemove } = props;
   const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
   const taxPrice = itemsPrice * 0.14;
-  const shippingPrice = itemsPrice > 75 ? 0 : 50;
+  const shippingPrice = itemsPrice > 50 ? 0 : 12;
   const totalPrice = itemsPrice + taxPrice + shippingPrice;
   return (
     <aside className="block col-1">
